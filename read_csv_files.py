eca7ca277b0755862de247ee3b1fa364a14af6c2
Ronaldo Caro <caroquiloronaldo@gmail.com>
2024-08-16 23:46:20 -0500

    Add files via upload

---
+++ read_csv_files.py
@@ -0,0 +1,17 @@
+# data from July 2023 to June 2024
+
+from pandas import read_csv
+july_23 = read_csv("./dataBicicletas/202307-divvy-tripdata.csv")  
+august_23 = read_csv("./dataBicicletas/202308-divvy-tripdata.csv")  
+sept_23 = read_csv("./dataBicicletas/202309-divvy-tripdata.csv")  
+oct_23 = read_csv("./dataBicicletas/202310-divvy-tripdata.csv")  
+nov_23 = read_csv("./dataBicicletas/202311-divvy-tripdata.csv")  
+dec_23 = read_csv("./dataBicicletas/202312-divvy-tripdata.csv")  
+
+january_24 = read_csv("./dataBicicletas/202401-divvy-tripdata.csv")
+feb_24 = read_csv("./dataBicicletas/202402-divvy-tripdata.csv")
+mar_24 = read_csv("./dataBicicletas/202403-divvy-tripdata.csv")
+apr_24 = read_csv("./dataBicicletas/202404-divvy-tripdata.csv")
+may_24 = read_csv("./dataBicicletas/202405-divvy-tripdata.csv")
+jun_24 = read_csv("./dataBicicletas/202406-divvy-tripdata.csv")
+
